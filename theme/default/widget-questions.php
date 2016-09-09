e35fb621a8cf18d71d0171c2cea552c3686c4844
Soumen Sarkar <soumenc3@gmail.com>
2016-09-09 23:20:48 +0530

    made widget font bold

--- theme/default/widget-questions.php
+++ theme/default/widget-questions.php
@@ -16,7 +16,7 @@
                                     <?php
                                         }
                                     ?>
-					<a class="ap-question-title"href="<?php ap_question_the_permalink() ?>"><?php the_title() ?></a>
+                                    <a style="font-size: bold;color: #333;" class="ap-question-title"href="<?php ap_question_the_permalink() ?>"><?php the_title() ?></a>
 					<span class="ap-ans-count"><?php printf( _n('1 answer', '%d answers', $ans_count, 'anspress-question-answer'), $ans_count) ?></span>
 					<span class="ap-vote-count"> | <?php printf( _n('1 vote', '%d votes', $net_vote, 'anspress-question-answer'), $net_vote) ?></span>
 				</div>
